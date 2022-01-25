87a857165c25c460429cf9b9a41feb9973d3abc4
DanielleIzawa <izawa.danielle@gmail.com>
2022-01-25 10:28:11 -0300

    Integração com o mocha

--- tests/server_http.test.js
+++ tests/server_http.test.js
@@ -1,13 +1,32 @@
 const assert = require('../src/core/assert/assert.js');
 const http = require('http');
+const { describe, it } = require('mocha');
 
-http.request('http://localhost:3000', (res) =>{
-  const chunks = [];
-  res.on('data', (chunk) => chunks.push(chunk));
-  res.on('end', () => {
-    const body = chunks.join('');
-    console.log('Integration test on localhost:3000 === Server On');
-    assert(body === 'Server On.');
+describe('Integration Tests', () => {
+  it('Integration test on localhost:3000 === Server On', () => {
+    http.request('http://localhost:3000', (res) => {
+      const chunks = [];
+      res.on('data', (chunk) => chunks.push(chunk));
+      res.on('end', () => {
+        const body = chunks.join('');
+        console.log('Integration test on localhost:3000 === Server On');
+        assert(body === 'Server On.');
+      });
+
+    }).end();
   });
 
-}).end();
+  it('Integration test on localhost:3000 !== Server On', () => {
+    http.request('http://localhost:3001', (res) => {
+      const chunks = [];
+      res.on('data', (chunk) => chunks.push(chunk));
+      res.on('end', () => {
+        const body = chunks.join('');
+        console.log('Integration test on localhost:3000 === Server On');
+        assert(body !== 'Server On.');
+      });
+
+    }).end();
+  })
+});
+
--- tests/unit_test.test.js
+++ tests/unit_test.test.js
@@ -1,8 +1,32 @@
-const assert = require('../src/core/assert/assert.js');
+const { describe, it } = require('mocha');
+/* const assert = require('../src/core/assert/assert.js');  */
+const assert = require('assert');
 const sqr = require('../src/core/sqr/sqr.js');
-var User = require('../src/core/user/user.js');
+/* var User = require('../src/core/user/user.js'); */
 
-function main() {
+describe('Testing of types and values', () => {
+  it('Testing string', () => {
+    assert('Pessoa' === 'Pessoa', 'Testing string');
+  });
+
+  it('Testing sqr', () => {
+    assert(sqr(3) === 9)
+  });
+
+  it('Testing sqr 2', () => {
+    assert(sqr(5) === 25)
+  });
+
+  it('Testing double', () => {
+    assert(0.0 === 0.0)
+  });
+/* 
+  it('Testing double', () => {
+    assert(0.0 === 1.0)
+  }); */
+});
+
+/* function main() {
   assert('Pessoa' === 'Pessoa', 'Testing string');
 
   assert(sqr(3) === 9, 'Testing sqr');
@@ -15,4 +39,4 @@ function main() {
 
 }
 
-main();
+main(); */
